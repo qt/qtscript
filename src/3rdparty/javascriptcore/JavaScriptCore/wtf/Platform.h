81993bab2cb1d37af4b9893ff10305920069b9e6
Allan Sandfeld Jensen <allan.jensen@digia.com>
2014-03-04 10:46:18 +0100

    Fix build on x32
    Recognize x32 and disable JIT in this configuration.
    
    Task-number: QTBUG-35463
    Change-Id: Ie5bf64f22f3e58a9b3f12190cf790ad6c39f415e
    Reviewed-by: Gregory Hinton Nietsky <gregory@distrotech.co.za>
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-by: Jocelyn Turcotte <jocelyn.turcotte@digia.com>

--- src/3rdparty/javascriptcore/JavaScriptCore/wtf/Platform.h
+++ src/3rdparty/javascriptcore/JavaScriptCore/wtf/Platform.h
@@ -202,6 +202,11 @@
 #if   defined(__x86_64__) \
     || defined(_M_X64)
 #define WTF_CPU_X86_64 1
+
+#if defined(__ILP32__)
+#define WTF_CPU_X32 1
+#endif
+
 #endif
 
 /* 64-bit mode on AIX */
@@ -906,7 +911,7 @@
 #endif
 
 #if !defined(WTF_USE_JSVALUE64) && !defined(WTF_USE_JSVALUE32) && !defined(WTF_USE_JSVALUE32_64)
-#if (CPU(X86_64) && (OS(UNIX) || OS(WINDOWS) || OS(SOLARIS) || OS(HPUX))) || (CPU(IA64) && !CPU(IA64_32)) || CPU(ALPHA) || CPU(AIX64) || CPU(SPARC64) || CPU(MIPS64) || CPU(AARCH64)
+#if (CPU(X86_64) && !CPU(X32) && (OS(UNIX) || OS(WINDOWS) || OS(SOLARIS) || OS(HPUX))) || (CPU(IA64) && !CPU(IA64_32)) || CPU(ALPHA) || CPU(AIX64) || CPU(SPARC64) || CPU(MIPS64) || CPU(AARCH64)
 #define WTF_USE_JSVALUE64 1
 #elif CPU(ARM) || CPU(PPC64)
 #define WTF_USE_JSVALUE32 1
@@ -921,6 +926,11 @@ on MinGW. See https://bugs.webkit.org/show_bug.cgi?id=29268 */
 
 #if !defined(ENABLE_REPAINT_THROTTLING)
 #define ENABLE_REPAINT_THROTTLING 0
+#endif
+
+/* Disable JIT on x32 */
+#if CPU(X32)
+#define ENABLE_JIT 0
 #endif
 
 #if !defined(ENABLE_JIT)
