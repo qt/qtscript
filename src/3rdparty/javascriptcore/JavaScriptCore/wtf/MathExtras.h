14710df8fa3b8c31cdbf0243d268a006e667e13a
Allan Sandfeld Jensen <allan.jensen@digia.com>
2014-09-29 11:06:17 +0200

    Fix QNX build
    Copy the MathExtra QNX workarounds from QtWebKit to QtScript.
    
    Change-Id: I95fbeeb6669a69e74f4b8a64b148dc5eff8c8e16
    Reviewed-by: Jani Heikkinen <jani.heikkinen@digia.com>
    Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

--- src/3rdparty/javascriptcore/JavaScriptCore/wtf/MathExtras.h
+++ src/3rdparty/javascriptcore/JavaScriptCore/wtf/MathExtras.h
@@ -40,6 +40,18 @@
 #include <machine/ieee.h>
 #endif
 
+#if OS(QNX)
+// FIXME: Look into a way to have cmath import its functions into both the standard and global
+// namespace. For now, we include math.h since the QNX cmath header only imports its functions
+// into the standard namespace.
+#include <math.h>
+// These macros from math.h conflict with the real functions in the std namespace.
+#undef signbit
+#undef isnan
+#undef isinf
+#undef isfinite
+#endif
+
 #ifndef M_PI
 const double piDouble = 3.14159265358979323846;
 const float piFloat = 3.14159265358979323846f;
