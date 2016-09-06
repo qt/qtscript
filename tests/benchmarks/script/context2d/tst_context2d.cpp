c2b958dcc02730e54bbaa271d21f88ccb5e6945c
Marc Mutz <marc.mutz@kdab.com>
2016-09-06 13:36:02 +0200

    Eradicate Q_FOREACH loops and mark the module as Q_FOREACH-free
    Change-Id: I13a4ce5c268da2b97cbae6612d240398b17186ef
    Reviewed-by: Jędrzej Nowacki <jedrzej.nowacki@theqtcompany.com>
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>

--- tests/benchmarks/script/context2d/tst_context2d.cpp
+++ tests/benchmarks/script/context2d/tst_context2d.cpp
@@ -104,8 +104,8 @@ void tst_Context2D::newEnvironment()
 void tst_Context2D::singleExecution_data()
 {
     QTest::addColumn<QString>("testName");
-    QFileInfoList testFileInfos = testsDir.entryInfoList(QStringList() << "*.js", QDir::Files);
-    foreach (QFileInfo tfi, testFileInfos) {
+    const QFileInfoList testFileInfos = testsDir.entryInfoList(QStringList() << "*.js", QDir::Files);
+    for (const QFileInfo &tfi : testFileInfos) {
         QString name = tfi.baseName();
         QTest::newRow(name.toLatin1().constData()) << name;
     }
@@ -136,8 +136,8 @@ void tst_Context2D::repeatedExecution_data()
     // us to observe potential effects of profiling-based JIT optimizations.
     QTest::addColumn<QString>("testName");
     QTest::addColumn<QString>("script");
-    QFileInfoList testFileInfos = testsDir.entryInfoList(QStringList() << "*.js", QDir::Files);
-    foreach (QFileInfo tfi, testFileInfos) {
+    const QFileInfoList testFileInfos = testsDir.entryInfoList(QStringList() << "*.js", QDir::Files);
+    for (const QFileInfo &tfi : testFileInfos) {
         QString script = readFile(tfi.absoluteFilePath());
         QString name = tfi.baseName();
         newEnvironment();
